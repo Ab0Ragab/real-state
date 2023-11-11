ac801497ee84099c44b678a72373d4e4a8be9ced
Ahmed Mohamed AboRagab <52207564+Ab0Ragab@users.noreply.github.com>
2023-11-11 07:21:08 +0200

    add users rates

--- src/components/rate/rate.js
+++ src/components/rate/rate.js
@@ -1,8 +1,42 @@
 import { ReactSVG } from "react-svg";
 import "./rate.scss";
 
-function Rate() {
-  return <div className="rate">dd</div>;
+function Rate({ rate }) {
+  const fullStars = [];
+  const emptyStars = [];
+
+  for (let i = 0; i < rate; i++) {
+    fullStars.push(<ReactSVG src="/icons/rate.svg" />);
+  }
+
+  for (let i = 0; i < 5 - rate; i++) {
+    emptyStars.push(<ReactSVG src="/icons/star.svg" />);
+  }
+
+  return (
+    <div className="rate">
+      <div className="rate-stars">
+        {fullStars}
+        {emptyStars}
+      </div>
+      <div className="rate-desc">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        varius enim in eros elementum tristique. Duis cursus, mi quis viverra
+        ornare."
+      </div>
+      <div className="rate-user d-flex flex-wrap align-items-center justify-content-between">
+        <div className="rate-user-info d-flex align-items-center">
+          <div className="rate-user-info-avatar">
+            <img src="/images/user1.png" alt="user" loading="lazy" />
+          </div>
+          <div>
+            <span className="rate-user-info-name d-block">Jenny Wilson</span>
+            <span className="rate-user-info-job">Agent</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Rate;
--- src/views/home/components/home-rates/home-rates.js
+++ src/views/home/components/home-rates/home-rates.js
@@ -1,14 +1,18 @@
+import React, { useRef } from "react";
+import { ReactSVG } from "react-svg";
 import Rate from "../../../../components/rate/rate";
-import { Navigation, Pagination } from "swiper/modules";
+import { Pagination } from "swiper/modules";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
-import "swiper/css/navigation";
 import "swiper/css/pagination";
 
+import "./home-rates.scss";
+
 function HomeRates() {
-  const rates = [{}, {}, {}, {}];
+  const swiperRef = useRef(null);
+  const rates = [{ rate: 4 }, { rate: 5 }, { rate: 3 }, { rate: 2 }];
   const breakpoints = {
     320: {
       slidesPerView: 1,
@@ -23,25 +27,45 @@ function HomeRates() {
       spaceBetween: 15,
     },
     1200: {
-      slidesPerView: 4,
+      slidesPerView: 3,
       spaceBetween: 30,
     },
   };
   return (
     <>
-      <Swiper
-        modules={[Navigation, Pagination]}
-        spaceBetween={50}
-        slidesPerView={3}
-        navigation
-        pagination={{ clickable: true }}
-        breakpoints={breakpoints}>
-        {rates.map((item, i) => (
-          <SwiperSlide key={i}>
-            <Rate />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className="rates">
+        <Swiper
+          ref={swiperRef}
+          modules={[Pagination]}
+          spaceBetween={50}
+          slidesPerView={3}
+          pagination={{
+            clickable: true,
+            el: ".pagination-bullets",
+          }}
+          breakpoints={breakpoints}>
+          {rates.map((item, i) => (
+            <SwiperSlide key={i}>
+              <Rate rate={item.rate} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className="rates-slider-btn d-flex justify-content-between">
+          <button
+            type="button"
+            className="slider-btn slider-btn-white"
+            onClick={() => swiperRef.current.swiper.slidePrev()}>
+            <ReactSVG src="icons/arrow-left.svg" />
+          </button>
+          <button
+            type="button"
+            className="slider-btn slider-btn-white"
+            onClick={() => swiperRef.current.swiper.slideNext()}>
+            <ReactSVG src="icons/right-arrow.svg" />
+          </button>
+        </div>
+      </div>
+      <div className="pagination-bullets text-center mt-5"></div>
     </>
   );
 }
